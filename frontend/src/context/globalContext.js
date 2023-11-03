9fe0125a518e36678bcf911cb8ccd695de1b82ea
dohoangnam2912 <dohoangnam2912@gmail.com>
2023-11-03 12:44:17 +0700

    website frontend

---
+++ frontend/src/context/globalContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GlobalContext = React.createContext();
+
+export const GlobalProvider = ({children}) => {
+    return (
+        <GlobalContext.Provider>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
